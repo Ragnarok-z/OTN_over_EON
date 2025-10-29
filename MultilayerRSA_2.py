c86a1b4cffaef5fa08ffead7cd2a5057fea90255
zhushen <2407396770@qq.com>
2025-10-29 18:20:43 +0800

    做了整理与不整理的两个实验exp63/64，

--- MultilayerRSA_2.py
+++ MultilayerRSA_2.py
@@ -29,13 +29,13 @@ def run_experiments(topology_file, output_dir="results"):
     random_seed = 423
 
     # Define policies to test
-    # policies = ["MinEn", "MaxMux", "MaxSE", "MinPB"]
-    policies = ["MaxMux", "MaxSE", "MinPB"]
+    policies = ["MinEn", "MaxMux", "MaxSE", "MinPB"]
+    # policies = ["MaxMux", "MaxSE", "MinPB"]
 
 
     # Define defragmentation params
     defarg_params = {
-        "en":False
+        "en":True
     }
 
     # Initialize results storage
@@ -55,7 +55,7 @@ def run_experiments(topology_file, output_dir="results"):
             print(f"Running simulations for traffic intensity: {intensity} Erlang")
 
             # Create a new simulator for each run to ensure clean state
-            simulator = Simulator(Network(topology_file), intensity, num_demands, random_seed, defarg_params, output_dir)
+            simulator = Simulator(Network(topology_file), intensity, num_demands, random_seed, defarg_params,output_dir)
             simulator.run(policy)
 
             # Store results
